28885482a0d71df202193147363967deb2476b3e
Yaduraj.doc <yadurajsingham@gmail.com>
2026-01-20 08:41:46 +0530

    Update asciicam to v1.0.1 (fix entry point)

--- Formula/asciicam.rb
+++ Formula/asciicam.rb
@@ -1,8 +1,8 @@
 class Asciicam < Formula
   desc "Real-time ASCII Camera for your terminal"
   homepage "https://github.com/YaduEnc/AsciiCam"
-  url "https://github.com/YaduEnc/ASCIICam/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e80cf9e1ccb232ff8f44a38e3e82573316e4aa43055a4b1a8f33b1a78a1cc112" # Calculate with: curl -L <url> | shasum -a 256
+  url "https://github.com/YaduEnc/ASCIICam/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "f754e0d5f35a0d18de176d8c0e3f98ae549d20f7058f1dff5f692751aea78c99"
   license "MIT"
 
   depends_on "python@3.11"
