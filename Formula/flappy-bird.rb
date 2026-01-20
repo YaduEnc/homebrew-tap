e4f0b7f5d56677ecf1ae0d196fab362660e3e06d
Yaduraj.doc <yaduraj.enc@gmail.com>
2026-01-20 08:37:33 +0530

    Update flappy-bird.rb

--- Formula/flappy-bird.rb
+++ Formula/flappy-bird.rb
@@ -1,19 +1,19 @@
-class FlappyBird < Formula
-  desc "A polished terminal-based Flappy Bird clone"
-  homepage "https://github.com/YaduEnc/FlappyBirdTUI"
-  url "https://github.com/YaduEnc/FlappyBirdTUI/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "392b396fb28c3ad6545c36b1b4a6bb70218203f4de1ac4fc9b48be4df20d5d20"
+class Asciicam < Formula
+  desc "Real-time ASCII Camera for your terminal"
+  homepage "https://github.com/YaduEnc/AsciiCam"
+  url "https://github.com/YaduEnc/ASCIICam/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "e80cf9e1ccb232ff8f44a38e3e82573316e4aa43055a4b1a8f33b1a78a1cc112" # Calculate with: curl -L <url> | shasum -a 256
   license "MIT"
 
   depends_on "python@3.11"
+
+  include Language::Python::Virtualenv
 
   def install
     virtualenv_install_with_resources
   end
 
-  include Language::Python::Virtualenv
-
   test do
-    system "#{bin}/flappy-bird", "--version"
+    system "#{bin}/asciicam", "--version"
   end
 end
